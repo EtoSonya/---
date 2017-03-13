8568a805cc725e85107b81c8ac1c44c3e5a2b22c
Тигран Ахинян <teagrow1@gmail.com>
2017-03-13 23:41:42 +0400

    Создал класс

---
+++ КДЗ по проге/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo
+{
+    class Достом
+    {
+
+        private string _название;
+
+        public string Название
+        {
+            get { return _название; }
+            set { _название = value; }
+        }
+
+        // 0 - 10
+        private int _адрес;
+        private static string адрес;
+
+        public int Адрес
+        {
+            get { return _адрес; }
+            set { _адрес = value; }
+        }
+
+        public Достом(string name, string adres)
+        {
+            _название = Название;
+            _адрес = Адрес;
+        }
+
+        public Достом(string name)
+            : this(name, адрес)
+        {
+        }
+    }
+}
